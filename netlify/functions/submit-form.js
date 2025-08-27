6ddb1a9b32dedeab21d1830f6efc091026ce1044
Usmart <usmart.v.ai@gmail.com>
2025-08-27 09:29:40 +0200

    Update submit-form.js
    Archivo actualizado

--- netlify/functions/submit-form.js
+++ netlify/functions/submit-form.js
@@ -1,9 +1,15 @@
 exports.handler = async (event, context) => {
   if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Método no permitido' };
+    return {
+      statusCode: 405,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ error: "Método no permitido" })
+    };
   }
+
   return {
     statusCode: 200,
-    body: 'Función de prueba recibida!'
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message: "Función de prueba recibida!" })
   };
 };
